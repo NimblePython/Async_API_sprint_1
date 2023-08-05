a2db8f67edadf8fe0ac4098f2923dae2e5e85e98
Alexander Lyotov <sippie84@gmail.com>
2023-08-06 00:10:37 +0300

    feature/модель данных персоны

---
+++ src/models/person.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""Модуль, где определена модель кинопроизведения."""
+from typing import List, Optional
+from uuid import UUID
+
+import orjson
+from pydantic import BaseModel
+
+
+class PortfolioFilm(BaseModel):
+    """Модель данных фильма в портфолио персоны."""
+
+    uuid: UUID
+    roles: List[str]
+    # я бы добавил title, но в техническом задании этого поля нет
+    # если брать аналогию с актёрами в фильмах - здесь должен быть именно title
+
+
+class Person(BaseModel):
+    """Модель данных персоны."""
+
+    uuid: UUID
+    full_name: str
+    films: Optional[List[PortfolioFilm]]
