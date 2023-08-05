7d1d7e93f69dd8dfccbe50613067e429890da6e6
Alexander Lyotov <sippie84@gmail.com>
2023-08-06 00:31:05 +0300

    feature/модели фильма используют BaseModelImproved

--- src/models/film.py
+++ src/models/film.py
@@ -3,60 +3,32 @@
 from typing import List, Optional
 from uuid import UUID
 
-import orjson
-# Используем pydantic для упрощения работы при перегонке данных из json в объекты
-from pydantic import BaseModel
+from src.models.base_model_improved import BaseModelImproved
 
 
-def orjson_dumps(value_to_dump, *, default):
-    """Конвертировать в передаваемое значение в json-строку.
-
-    "*" - обозначает конец позиционных аргументов (PEP 3102)
-    To serialize a subclass or arbitrary types, specify default as a callable
-    that returns a supported type. specify default as a callable that returns a supported type.
-
-    Parameters:
-        value_to_dump: значение, которое нужно конвертировать
-        default: default may be a function, lambda, or callable class instance
-
-
-    Returns:
-        json_str: строка в формате json
-    """
-    # orjson.dumps возвращает bytes, а pydantic требует unicode, поэтому декодируем
-    return orjson.dumps(value_to_dump, default=default).decode()
-
-
-class Participant(BaseModel):
+class Participant(BaseModelImproved):
     """Модель данных персоны, участвующей в создании фильма."""
 
     id: UUID
     name: str
 
 
-class FilmGenre(BaseModel):  # TODO стоит ли наследовать модели от общего класса?
+class FilmGenre(BaseModelImproved):  # TODO стоит ли наследовать модели от общего класса?
     """Модель данных жанра, к которому относится фильм."""
 
     id: UUID
     name: str
 
 
-class Film(BaseModel):
+class Film(BaseModelImproved):
     """Модель данных кинопроизведения (минимальная - для главной страницы)."""
 
     id: UUID
     title: str
     imdb_rating: float
 
-    class Config(object):
-        """Класс конфигурации модели Pydantic."""
 
-        # Заменяем стандартную работу с json на более быструю
-        json_loads = orjson.loads
-        json_dumps = orjson_dumps
-
-
-class FilmDetailed(BaseModel):
+class FilmDetailed(Film):
     """Схема данных подробностей о кинопроизведении."""
 
     genre: Optional[List[FilmGenre]]  # TODO: уточнить, т.к.
