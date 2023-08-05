1208873712adb364b24aa14a3d04a311139d6400
Alexander Lyotov <sippie84@gmail.com>
2023-08-06 00:28:32 +0300

    feature/улучшенная (orjson) BaseModel

---
+++ src/models/base_model_improved.py
@@ -0,0 +1,33 @@
+"""Модуль, где задаётся модель Pydantic, использующая для сериализации и десериализации orjson."""
+import orjson
+from pydantic import BaseModel
+
+
+def orjson_dumps(value_to_dump, *, default):
+    """Конвертировать в передаваемое значение в json-строку.
+
+    "*" - обозначает конец позиционных аргументов (PEP 3102)
+    To serialize a subclass or arbitrary types, specify default as a callable
+    that returns a supported type. specify default as a callable that returns a supported type.
+
+    Parameters:
+        value_to_dump: значение, которое нужно конвертировать
+        default: default may be a function, lambda, or callable class instance
+
+
+    Returns:
+        json_str: строка в формате json
+    """
+    # orjson.dumps возвращает bytes, а pydantic требует unicode, поэтому декодируем
+    return orjson.dumps(value_to_dump, default=default).decode()
+
+
+class BaseModelImproved(BaseModel):
+    """Модель данных кинопроизведения (минимальная - для главной страницы)."""
+
+    class Config(object):
+        """Класс конфигурации модели Pydantic."""
+
+        # Заменяем стандартную работу с json на более быструю
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
