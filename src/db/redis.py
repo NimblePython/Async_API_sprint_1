8db057a0e4bde67e22596d9a84c6189a34f282d3
Alexander Lyotov <alexander.lyotov@yandex.ru>
2023-08-09 01:04:34 +0300

    feature/доделал инициализацию redis

--- src/db/redis.py
+++ src/db/redis.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 from typing import Optional
+
 from redis.asyncio import Redis
 
-redis: Optional[Redis] = None
+from src.core.config import REDIS_HOST, REDIS_PORT
+
+redis: Optional[Redis] = Redis(
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+)
 
 # Функция понадобится при внедрении зависимостей
 async def get_redis() -> Redis:
