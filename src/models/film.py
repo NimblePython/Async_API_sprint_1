c4f189703f462e000b9cb81a0a72dd13e97807a7
Alexander Lyotov <alexander.lyotov@yandex.ru>
2023-08-12 15:31:19 +0300

    feature/заменил id на uuid в схеме фильма

--- src/models/film.py
+++ src/models/film.py
@@ -9,21 +9,21 @@ from src.models.base_model_improved import BaseModelImproved
 class Participant(BaseModelImproved):
     """Модель данных персоны, участвующей в создании фильма."""
 
-    id: UUID
+    uuid: UUID
     name: str
 
 
 class FilmGenre(BaseModelImproved):
     """Модель данных жанра, к которому относится фильм."""
 
-    id: UUID
+    uuid: UUID
     name: str
 
 
 class Film(BaseModelImproved):
     """Модель данных кинопроизведения (минимальная - для главной страницы)."""
 
-    id: UUID
+    uuid: UUID
     title: str
     imdb_rating: float
 
