e52346fea53ebe87d936ad7c48ffe5fad38aa7fd
Alexander Lyotov <alexander.lyotov@yandex.ru>
2023-08-19 18:22:30 +0300

    feature/комментарий с мотивировкой удаления class Config из config'а

--- src/core/config.py
+++ src/core/config.py
@@ -19,8 +19,22 @@ class Settings(BaseSettings):
     # Время жизни кэша Redis
     CACHE_TIME_LIFE: int = Field(env='CACHE_TIME_LIFE')
 
-    class Config:
-        extra = Extra.ignore
+    # Далее я закомментировал аналог того, что делает .env dotenv.
+    # Нам это не нужно, т.к. предполагаем, что у нас все переменные уже загружены в окружение:
+    # при локальном запуске - средствами IDE (.env.local),
+    # при запуске в docker-compose - средствами docker-compose (.env)
+
+    # в pycharm'е могут быть проблемы с загрузкой .env.local в переменные окружения:
+    # из коробки pycharm в режиме debug не позволяет этого делать, позволяет лишь задавать перемен-
+    # ные поштучно. Существует плагин с названием типа "envfile".
+
+    # Если запуск в режиме debug в pycharm (обозначенным выше способом) останется проблемой,
+    # можем вернуться к рассмотрению конструкции ниже и решить, как динамически задавать
+    # env_file
+
+    # > class Config:
+    # >    env_file = '.env'
+    # >    extra = Extra.ignore
 
 
 settings = Settings()
