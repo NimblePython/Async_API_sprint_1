63edff0962c4a25853846928bd81234711ad799e
Alexander Lyotov <alexander.lyotov@yandex.ru>
2023-08-19 16:51:25 +0300

    feature/убрал лишний код из config

--- src/core/config.py
+++ src/core/config.py
@@ -1,9 +1,7 @@
-# -*- coding: utf-8 -*-
-import logging
-import os
+
 from logging import config as logging_config
 
-from pydantic import Field, Extra, ValidationError
+from pydantic import Extra, Field
 from pydantic_settings import BaseSettings
 
 from src.core.logger import LOGGING
@@ -22,30 +20,10 @@ class Settings(BaseSettings):
     CACHE_TIME_LIFE: int = Field(env='CACHE_TIME_LIFE')
 
     class Config:
-        env_file = '.env'
         extra = Extra.ignore
 
 
 settings = Settings()
-print(settings.model_dump())
-
 
 # Применяем настройки логирования
 logging_config.dictConfig(LOGGING)
-
-""" TODO: удалить блок при успешной работе класса конфигурации
-
-# Название проекта. Используется в Swagger-документации
-PROJECT_NAME = os.getenv('PROJECT_NAME', 'movies')
-
-# Настройки Redis
-REDIS_HOST = os.getenv('REDIS_HOST', 'redis')
-REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
-
-# Настройки Elasticsearch
-ELASTIC_HOST = os.getenv('ELASTIC_HOST', 'es')
-ELASTIC_PORT = int(os.getenv('ELASTIC_PORT', 9200))
-"""
-
-# Корень проекта
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
