ba012c82f612f00f65d02120698bbcce93b891dd
NimblePython <safaroff2@gmail.com>
2023-08-24 22:23:10 +0300

    refactor/flake8 and string formatting

--- postgres_to_es/backoff_dec.py
+++ postgres_to_es/backoff_dec.py
@@ -1,17 +1,13 @@
-"""Параметрический декоратор для осуществления
-экспоненциально растущей паузы между повторными
-вызовами декорируемой функции.
-"""
+"""Параметрический декоратор для осуществления паузы между повторными вызовами функции."""
 
-import time
 import logging
-
+import time
 from functools import wraps
 
 
 def backoff(start_sleep_time=0.1, factor=2, border_sleep_time=10):
-    """
-    Функция для повторного выполнения функции через некоторое время, если возникла ошибка.
+    """Декоратор для паузы между вызовами одной и той же функции в случае ошибки.
+
     Использует наивный экспоненциальный рост времени повтора (factor)
     до граничного времени ожидания (border_sleep_time)
 
@@ -19,29 +15,29 @@ def backoff(start_sleep_time=0.1, factor=2, border_sleep_time=10):
         t = start_sleep_time * 2^(n) if t < border_sleep_time
         t = border_sleep_time if t >= border_sleep_time
 
+    Args:
+        start_sleep_time: начальное время повтора
+        factor: во сколько раз нужно увеличить время ожидания
+        border_sleep_time: граничное время ожидания
 
-    :param start_sleep_time: начальное время повтора
-    :param factor: во сколько раз нужно увеличить время ожидания
-    :param border_sleep_time: граничное время ожидания
+    Returns:
+        Функция-обёртка
     """
-
     def func_wrapper(func):
         @wraps(func)
         def inner(*args, **kwargs):
-            logging.debug(f"Выполнение функции {func.__name__}")
-            n = 0
+            logging.debug('Выполнение функции %s' % func.__name__)
+            pwr = 0
             while True:
                 try:
                     return func(*args, **kwargs)
-                except Exception as e:
-                    logging.warning(f"Ошибка выполнения функции {func.__name__}")
-                    n += 1
-                    t = start_sleep_time * factor**n
-                    if t >= border_sleep_time:
-                        t = border_sleep_time
-                    logging.debug(f"Пауза до следующего выполнения функции: {t} сек")
-                    time.sleep(t)
+                except Exception as err:
+                    logging.warning('Ошибка %s выполнения функции %s' % (err, func.__name__))
+                    pwr += 1
+                    pause = start_sleep_time * factor**pwr
+                    if pause >= border_sleep_time:
+                        pause = border_sleep_time
+                    logging.debug('Пауза до следующего выполнения функции: %s сек' % pause)
+                    time.sleep(pause)
         return inner
-
     return func_wrapper
-
